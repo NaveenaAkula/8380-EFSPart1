6e44aca67246d9fdd2fc432e61fba041dbd017dd
unknown <jakula@unomaha.edu>
2020-07-17 17:28:04 -0500

    add files

--- efs/settings.py
+++ efs/settings.py
@@ -130,7 +130,6 @@ LOGIN_REDIRECT_URL = '/home'
 LOGOUT_REDIRECT_URL = '/accounts/login'
 STATIC_URL = '/static/'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
-
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'adapanaveena2526@gmail.com'
@@ -139,6 +138,7 @@ EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 DEFAULT_FROM_EMAIL = 'test team <adapanaveena2526@gmail.com>'
 
+<<<<<<< HEAD
 django_heroku.settings(locals())
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
@@ -147,3 +147,5 @@ STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL ='/media/'
 
+=======
+>>>>>>> 5b1d31ff970cdea837d7f027020fcca1349590a9
